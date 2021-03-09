497b19c16991d4a5496aebb2860799560928fbbc
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2021-03-09 14:01:26 +0100

    Adding the map with Leaflet.js

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+// DOM Elements
+var map = L.map("map-container").setView([51.505, -0.09], 13);
+
+//change pos
+// map.setView([60.505, -10.09], 13);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
 async function getIpData() {
   //Get the IPFY API key from the token-hider file
   const tokenRequest = await fetch("/.netlify/functions/token-hider");
