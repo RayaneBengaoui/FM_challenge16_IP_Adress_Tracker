1b9c94ceea2d5f5819836b9e179e570a86950f53
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2021-03-10 17:12:33 +0100

    Fetching personnal IP + updating the map location

--- app.js
+++ app.js
@@ -9,24 +9,47 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-async function getIpData() {
+function setMapLocation(map, lat, lng) {
+  map.setView([lat, lng], 13);
+}
+
+async function getToken() {
   //Get the IPFY API key from the token-hider file
   const tokenRequest = await fetch("/.netlify/functions/token-hider");
   const { key } = await tokenRequest.json();
-  let ipfy_fetch_link = `
-https://geo.ipify.org/api/v1?apiKey=${key}&ipAddress=8.8.8.8`;
+
+  return key;
+}
+
+async function getOwnIp() {
+  const fetch_link = `https://api.ipify.org?format=json`;
+  const response = await fetch(fetch_link);
+  const { ip } = await response.json();
+  return ip;
+}
+
+async function getIpData(ip) {
+  const key = await getToken();
+  console.log("");
+
+  if (ip === undefined) {
+    ip = await getOwnIp();
+  }
+
+  let ipfy_fetch_link = `https://geo.ipify.org/api/v1?apiKey=${key}&ipAddress=${ip}`;
 
   // Fetching the IPFY API with the given IP adress
   const ipfyRequest = await fetch(ipfy_fetch_link);
   const {
-    location: { country, region, city, timezone },
+    location: { country, region, city, timezone, lat, lng },
     isp,
   } = await ipfyRequest.json();
 
-  console.log(isp);
+  setMapLocation(map, lat, lng);
 }
 
 getIpData();
+// getOwnIp();
 
 let ip_adress = "";
 
